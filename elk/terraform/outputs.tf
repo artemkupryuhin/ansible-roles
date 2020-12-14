b5194d49335343b1a9c8ce231724ecac49645c55
artemkupruhin <artemkupruhin@gmail.com>
2020-12-14 21:24:41 +0200

    fix typo

--- elk/terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "elasticsearch-names" {
-  value = "${aws_instance.ec2-elasticsearch.*.tags.Name}"
-}
-output "elasticsearch-public_ips" {
-  value = "${aws_instance.ec2-elasticsearch.*.public_ip}"
-}
-output "elasticsearch-private_ips" {
-  value = "${aws_instance.ec2-elasticsearch.*.private_ip}"
-}
